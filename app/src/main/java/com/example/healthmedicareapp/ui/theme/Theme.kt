44cef42f9c2f5e5b7c40c19e39fb849c05420e33
pratish444 <pratishk444@gmail.com>
2025-08-08 22:29:40 +0530

    updated

--- app/src/main/java/com/example/healthmedicareapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/healthmedicareapp/ui/theme/Theme.kt
@@ -1,58 +1,67 @@
 package com.example.healthmedicareapp.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Typography
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
-
-@Composable
-fun HealthMediCareAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
     )
-}
+)
+
+val Activityscreen = Modifier.fillMaxWidth().fillMaxSize().background(color = Color.White).padding(25.dp)
+val fillmaxwid = Modifier.fillMaxWidth()
+val wid150 = Modifier.width(150.dp)
+val wid220 = Modifier.width(220.dp)
+val horzcenter = Alignment.CenterHorizontally
+val horzspacear = Arrangement.SpaceAround
+val horzstart = Arrangement.Start
+val vertspace=Arrangement.SpaceEvenly
+
+val boxes= Modifier.fillMaxWidth().border(BorderStroke(2.dp, Color(0xFF673AB7)))
+
+val rcshape= RoundedCornerShape(5.dp)
+val iconsize = Modifier.size(35.dp)
+val icon = Modifier.size(30.dp)
+
+val reglogbuttxtcol = Color.White
+val subtxtcol = Color.DarkGray
+
+val reglogbut = 20.sp
+val subtxtsize = 16.sp
+val fnt24 = 24.sp
+val fnt23 = 23.sp
+val fnt21 = 21.sp
+val fnt20 = 20.sp
+val fnt19 = 19.sp
+val fnt18 = 18.sp
+
+val txtbold = FontWeight.Bold
+var txtcenter = TextAlign.Center
+
+val mobkeypad = KeyboardOptions(keyboardType = KeyboardType.Phone)
