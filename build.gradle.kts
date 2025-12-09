2841bd9110650e054eb80d4b115028e89772e0a6
pratish444 <pratishk444@gmail.com>
2025-12-10 02:00:28 +0530

    Update Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
-    kotlin("kapt") apply false
-}
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        // If you ever need to add classpath dependencies (for legacy builds), add them here
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
