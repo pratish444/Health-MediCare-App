278f7e26b25e056bcb27e63beb838175ec025e21
pratish444 <pratishk444@gmail.com>
2025-12-12 00:08:47 +0530

    Create MedicalRepository.kt

---
+++ app/src/main/java/com/example/healthmedicareapp/domain/repository/MedicalRepository.kt
@@ -0,0 +1,9 @@
+package com.example.healthmedicareapp.domain.repository
+
+import com.example.healthmedicareapp.domain.model.MedicalDetails
+import kotlinx.coroutines.flow.Flow
+
+interface MedicalRepository {
+    suspend fun saveMedicalDetails(details: MedicalDetails): Result
+    fun getMedicalDetails(userId: String): Flow
+}
