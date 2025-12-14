7d66cd941cf4410b13bf29ee34402558473bc853
pratish444 <pratishk444@gmail.com>
2025-12-14 23:04:30 +0530

    Created RepositoryModule

---
+++ app/src/main/java/com/example/healthmedicareapp/domain/usecase/SignInUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.healthmedicareapp.domain.usecase
+
+import com.example.healthmedicareapp.domain.model.User
+import com.example.healthmedicareapp.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class SignInUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(email: String, password: String): Result {
+        return authRepository.signIn(email, password)
+    }
+}
