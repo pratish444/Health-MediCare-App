6071a1dcc2083c9cc4a45ce025fc479bccd22f94
pratish444 <pratishk444@gmail.com>
2026-01-27 13:25:46 +0530

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
 
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
+    implementation(libs.androidx.compose.material3.android)
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
