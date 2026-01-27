84c60d130b7da2b55bcdfb3270752d3425574793
pratish444 <pratishk444@gmail.com>
2026-01-27 13:56:53 +0530

    Updated MedicalRepository function

--- app/src/main/java/com/example/healthmedicareapp/domain/repository/SleepTrackRepository.kt
+++ app/src/main/java/com/example/healthmedicareapp/domain/repository/SleepTrackRepository.kt
@@ -4,8 +4,8 @@ import com.example.healthmedicareapp.domain.model.SleepTrack
 import kotlinx.coroutines.flow.Flow
 
 interface SleepTrackRepository {
-    suspend fun startSleepTracking(userId: String, startTime: Long, date: String): Result
-    suspend fun stopSleepTracking(id: Long, endTime: Long): Result
-    fun getAllSleepTracks(userId: String): Flow<List>
+    suspend fun startSleepTracking(userId: String, startTime: Long, date: String): Result<Unit>
+    suspend fun stopSleepTracking(id: Long, endTime: Long): Result<Unit>
+    fun getAllSleepTracks(userId: String): Flow<List<SleepTrack>>
     suspend fun getSleepTrackByDate(userId: String, date: String): SleepTrack?
 }
